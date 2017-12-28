af0da977dab01902a06e6ca1d3454a14d6927456
Shikan Chen <github.shikanchen@gmail.com>
2017-12-28 15:27:56 -0500

    encapsulated GBIManager

---
+++ GBIManager.py
@@ -0,0 +1,32 @@
+import urllib.request
+import json
+
+
+class GBIManager(object):
+    def __init__(self, fname):
+        self.base = fname
+
+    def get_data(self):
+        product = [x.strip().split(',')[0] for x in open(self.base)]
+        data_ticker = json.loads(urllib.request.urlopen('https://api.coinmarketcap.com/v1/ticker/').read())
+        data_available_ticker = [x for x in data_ticker if x['name'] in product]
+        total_cap = sum([float(x['market_cap_usd']) for x in data_ticker if x['name'] in product])
+        return total_cap, data_available_ticker
+
+    def get_weighted_base(self):
+        base = 0
+        for d in [x.strip().split(',') for x in open(self.base)]:
+            base += float(d[2]) * float(d[1])
+        return base
+
+    def calculate_gbi(self):
+        total_market_cap_usd, data = self.get_data()
+        weighted_avg = 0
+        for d in data:
+            cap_usd = float(d['market_cap_usd'])
+            for d2 in [x.strip().split(',') for x in open(self.base)]:
+                if d['name'] == d2[0]:
+                    ratio = float(d2[1])
+            weighted_avg += cap_usd * ratio
+        self.gbi = weighted_avg / self.get_weighted_base() * 1000
+        return self.gbi
--- main.py
+++ main.py
@@ -1,36 +1,8 @@
-import urllib.request
-import json
-
-
-def get_data():
-    product = [x.strip().split(',')[0] for x in open(fbase)]
-    data_ticker = json.loads(urllib.request.urlopen('https://api.coinmarketcap.com/v1/ticker/').read())
-    data_available_ticker = [x for x in data_ticker if x['name'] in product]
-    total_cap = sum([float(x['market_cap_usd']) for x in data_ticker if x['name'] in product])
-    return total_cap, data_available_ticker
-
-
-def get_weighted_base():
-    base = 0
-    for d in [x.strip().split(',') for x in open(fbase)]:
-        base += float(d[2])*float(d[1])
-    return base
-
-
-def calculate_gbi():
-    total_market_cap_usd, data = get_data()
-    weighted_avg = 0
-    for d in data:
-        cap_usd = float(d['market_cap_usd'])
-        for d2 in [x.strip().split(',') for x in open(fbase)]:
-            if d['name'] == d2[0]:
-                ratio = float(d2[1])
-        weighted_avg += cap_usd * ratio
-    return weighted_avg / get_weighted_base() * 1000
-
+from GBIManager import *
 
 if __name__ == "__main__":
     fbase = 'base'
-    gbi = calculate_gbi()
+    gbiManager = GBIManager(fbase)
+    gbi = gbiManager.calculate_gbi()
     # error range 1.5%
     print('GBI:', gbi)
