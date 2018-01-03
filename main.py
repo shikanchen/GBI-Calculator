4510d2c8e7f20abd0471e04afc5bb6e944177ded
Shikan Chen <github.shikanchen@gmail.com>
2018-01-03 16:03:02 -0500

    add source binance

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from GBIManager import *
 from pymongo import MongoClient
 
 def insert():
-    updated_time, gbi = gbiManager.calculate_gbi()
     data = {
-        'updated_time': updated_time,
-        'bgi': gbi
+        'bgi': json.dumps(gbiManager.gbi)
     }
-    result = client.gbi.data.insert_one(data)
+    result = client.gbi.coinmarketcap.insert_one(data)
     print('inserted id: {}'.format(result.inserted_id))
 
 if __name__ == "__main__":
     fbase = 'base'
     gbiManager = GBIManager(fbase)
+    gbiManager.calculate_gbi()
     client = MongoClient('localhost', 27017)
     insert()
