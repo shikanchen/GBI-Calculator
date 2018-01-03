a59166f5eb2ffe90beeb3b4e5bbcd6a29b1bdff0
Shikan Chen <github.shikanchen@gmail.com>
2018-01-03 16:21:40 -0500

    Revert "add source binance"
    This reverts commit 6114112e15f844e9adb885edbef5e43ba3da4d51.

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from GBIManager import *
 from pymongo import MongoClient
 
 def insert():
+    updated_time, gbi = gbiManager.calculate_gbi()
     data = {
-        'bgi': json.dumps(gbiManager.gbi)
+        'updated_time': updated_time,
+        'bgi': gbi
     }
-    result = client.gbi.coinmarketcap.insert_one(data)
+    result = client.gbi.data.insert_one(data)
     print('inserted id: {}'.format(result.inserted_id))
 
 if __name__ == "__main__":
     fbase = 'base'
     gbiManager = GBIManager(fbase)
-    gbiManager.calculate_gbi()
     client = MongoClient('localhost', 27017)
     insert()
