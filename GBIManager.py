1cdab98d1ce052534b96e7fb482904da199f1edb
Shikan Chen <github.shikanchen@gmail.com>
2018-01-04 18:00:57 -0500

    multi source binance separated

--- GBIManager.py
+++ GBIManager.py
@@ -36,44 +36,35 @@ class GBIManager(object):
             for d2 in [x.strip().split(',') for x in open(self.base)]:
                 if d['symbol'] == d2[0]:
                     ratio = float(d2[1])
-            ftmp.write(','.join([d['symbol'],str(cap_usd*ratio), d['total_supply']]) + '\n')
+            ftmp.write(','.join([d['symbol'],str(cap_usd),d['total_supply'],str(ratio)]) + '\n')
             weighted_avg += cap_usd * ratio
         ftmp.close()
-        print(d2)
         self.gbi['coinmarketcap'] = time, weighted_avg / self.get_weighted_base() * 1000
 
     def source_binance(self, product):
         weighted_avg = 0
         for p in product:
             if p == 'BTC':
-                d2 = list(filter(lambda x: x[0] == p, [x.strip().split(',') for x in open(self.base)]))[0]
-                print(d2)
-                ratio = float(d2[1])
+                t = list(filter(lambda x: x[0] == p, [x.strip().split(',') for x in open(self.tmp)]))[0]
+                ratio = float(t[3])
                 BTC_price =json.loads(urllib.request.urlopen('https://api.binance.com/api/v1/trades?symbol='+p+'USDT').read())[-1]['price']
                 BTC_price = float(BTC_price)
-                qty = json.loads(urllib.request.urlopen('https://api.binance.com/api/v1/ticker/24hr?symbol='+p+'USDT').read())['volume']
-                print(qty)
-                cap_usd = BTC_price * float(qty)
+                cap_usd = BTC_price * float(t[2])
                 weighted_avg += cap_usd * ratio
                 continue
 
             try:
-                qty = json.loads(
-                    urllib.request.urlopen('https://api.binance.com/api/v1/ticker/24hr?symbol=' + p + 'BTC').read())[
-                    'volume']
                 price = json.loads(urllib.request.urlopen('https://api.binance.com/api/v1/trades?symbol='+p+'BTC').read())[-1]['price']
             except HTTPError:
                 for t in [x.strip().split(',') for x in open(self.tmp)]:
                     if t[0] == p:
-                        weighted_avg += float(t[1])
+                        weighted_avg += float(t[1]) * float(t[3])
                 continue
 
             for t in [x.strip().split(',') for x in open(self.tmp)]:
                 if p == t[0]:
                     cap_usd = float(price) * BTC_price * float(t[2])
-            for d2 in [x.strip().split(',') for x in open(self.base)]:
-                if p == d2[0]:
-                    ratio = float(d2[1])
+                    ratio = float(t[3])
             weighted_avg += cap_usd * ratio
             time = json.loads(urllib.request.urlopen('https://api.binance.com/api/v1/trades?symbol='+p+'BTC').read())[-1]['time']
         self.gbi['binance'] = time, weighted_avg / self.get_weighted_base() * 1000
