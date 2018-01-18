201c6ce63c4ed949029e2cf50f4987738507172a
Shikan Chen <github.shikanchen@gmail.com>
2018-01-17 23:09:57 -0500

    update readme

--- README.md
+++ README.md
@@ -28,6 +28,10 @@ Result
 ```Shell
 {'coinmarketcap': (1515191961, 21267.434354247067), 'binance': (1515192129499, 20622.02891096077), 'kraken': (1515191961, 21124.81255298973)}
 ```
+Source List
+- [x] coinmarketcap
+- [x] binance
+- [x] kraken
 ### Use Crontab to run extension schedulely
 
 Type the following command to enter cronjob:
--- main.py
+++ main.py
@@ -14,10 +14,6 @@ def insert():
 if __name__ == "__main__":
     fbase = 'base'
     gbiManager = GBIManager(fbase)
-    # source list
-    #   `coinmarketcap`
-    #   `binance`
-    #   `kraken`
     gbiManager.calculate_gbi(['coinmarketcap', 'binance', 'kraken'])
     print(gbiManager.gbi)
     client = MongoClient('localhost', 27017)
